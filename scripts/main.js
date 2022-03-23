83ace49f699268a2f3ee7c6950ec1e9261a4d36a
Usman Arshad <uarshad2012@gmail.com>
2022-03-23 08:31:33 -0700

    up to saved page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);   //global
+        console.log(currentUser);
+
+        // the following functions are always called when someone is logged in
+        read_display_Quote();
+        insertName();
+        populateCardsDynamically();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+
+
 //----------------------------------------------------------
 // This is a function that gets called everytime the page loads
 // to display a quote of the day.  At the moment, it only displays
@@ -8,36 +26,30 @@ function read_display_Quote(){
     //console.log("inside the function")
 
     //get into the right collection
-    db.collection("quotes").doc("tuesday")
+    db.collection("quotes").doc("Tuesday")
     .onSnapshot(function(tuesdayDoc) {
         //console.log(tuesdayDoc.data());
         document.getElementById("quote-goes-here").innerHTML=tuesdayDoc.data().quote;
     })
 }
-read_display_Quote();
+// read_display_Quote();
 
 //---------------------------------------------------------------------------
 // This is a function that gets called everytime the page loads.
 // It is meant to get the name of the user who is logged in, and insert it 
 // on the page for a warm welcome. 
 //---------------------------------------------------------------------------
-function insertName(){
-// to check if the user is logged in:
- firebase.auth().onAuthStateChanged(user =>{
-     if (user){
-         console.log(user.uid); // let me to know who is the user that logged in to get the UID
-        currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-        currentUser.get().then(userDoc=>{
-            //get the user name
-            var user_Name= userDoc.data().name;
-            console.log(user_Name);
-            $("#name-goes-here").text(user_Name); //jquery
-            // document.getElementByID("name-goes-here").innetText=user_Name;
-        })    
-    }
- })
+// Insert name function using the global variable "currentUser"
+function insertName() {
+    currentUser.get().then(userDoc => {
+        //get the user name
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        $("#name-goes-here").text(user_Name); //jquery
+        // document.getElementByID("name-goes-here").innetText=user_Name;
+    })
 }
-insertName();
+// insertName();
 
 //--------------------------------------------------------------------------
 // This is a function that we call only ONE time, to populate the database.
@@ -55,8 +67,9 @@ function writeHikes() {
         city: "Burnaby",
         province: "BC",
         level: "easy",
-        length: "10 km",
-        length_time: "2h 33mm"
+        length: 10,          //number value
+        length_time: 60,     //number value
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
     });
     hikesRef.add({
         id: "AM01",
@@ -64,17 +77,19 @@ function writeHikes() {
         city: "Anmore",
         province: "BC",
         level: "moderate",
-        length: "10.5 km",
-        length_time: "3h 17mm"
+        length: 10.5,      //number value
+        length_time: 80,   //number value
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
     });
     hikesRef.add({
         id: "NV01",
-        name: "Mount Seymoure Trail", //replace with your own city?
+        name: "Mount Seymour Trail", //replace with your own city?
         city: "North Vancouver",
         province: "BC",
         level: "hard",
-        length: "8.2 km",
-        length_time: "3h 20m"
+        length: 8.2,        //number value
+        length_time: 120,   //number value
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2022"))
     });
 }
 
@@ -87,8 +102,11 @@ function writeHikes() {
 function populateCardsDynamically() {
     let hikeCardTemplate = document.getElementById("hikeCardTemplate");
     let hikeCardGroup = document.getElementById("hikeCardGroup");
-    
-    db.collection("Hikes").get()
+
+    db.collection("Hikes")
+        .orderBy("length_time")            //NEW LINE;  what do you want to sort by?
+        .limit(2)                       //NEW LINE:  how many do you want to get?
+        .get()
         .then(allHikes => {
             allHikes.forEach(doc => {
                 var hikeName = doc.data().name; //gets the name field
@@ -96,15 +114,44 @@ function populateCardsDynamically() {
                 var hikeLength = doc.data().length; //gets the length field
                 let testHikeCard = hikeCardTemplate.content.cloneNode(true);
                 testHikeCard.querySelector('.card-title').innerHTML = hikeName;
-                testHikeCard.querySelector('.card-length').innerHTML = hikeLength;
+	                    
+                //NEW LINE: update to display length, duration, last updated
+                testHikeCard.querySelector('.card-length').innerHTML = 
+                "Length: " + doc.data().length + " km <br>" +
+                "Duration: " + doc.data().length_time + "min <br>" +
+                "Last updated: " + doc.data().last_updated.toDate(); 
+
                 testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);
+
+                //next 2 lines are new for demo#11
+                //this line sets the id attribute for the <i> tag in the format of "save-hikdID" 
+                //so later we know which hike to bookmark based on which hike was clicked
+                //ps. this works because we have only one icon.
+                //if you have other icons, you will need a unique selector
+                testHikeCard.querySelector('i').id = 'save-' + hikeID;
+                // this line will call a function to save the hikes to the user's document             
+                testHikeCard.querySelector('i').onclick = () => saveBookmark(hikeID);
+
                 testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;
                 hikeCardGroup.appendChild(testHikeCard);
             })
-
         })
 }
-populateCardsDynamically();
+// populateCardsDynamically();
+
+function saveBookmark(hikeID) {
+    currentUser.set({
+            bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeID)
+        }, {
+            merge: true
+        })
+        .then(function () {
+            console.log("bookmark has been saved for: " + currentUser);
+            var iconID = 'save-' + hikeID;
+            //console.log(iconID);
+            document.getElementById(iconID).innerText = 'bookmark';
+        });
+}
 
 //--------------------------------------------------------------
 // This function saves the current hikeID into the localStorage
